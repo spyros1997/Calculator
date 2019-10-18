8be0408f51e6fb08993bd64bdd55c9fb32c238a5
mrjane7 <pennasgiann@gmailcom>
2019-10-18 22:33:17 +0300

    oloklirosh_button_decimalPoint

--- app/src/main/java/com/example/calculator3/MainActivity.java
+++ app/src/main/java/com/example/calculator3/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity<decimalFormat> extends AppCompatActivity {
 //δήλωση των κουμπιών και των textview
     private Button one;
     private Button two;
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private Button ison;
     private Button clear;
     private Button dia;
+    private Button comma;
     private TextView episkopish;
     private TextView apotelesma;
     private TextView episkopish2;
@@ -33,6 +34,11 @@ public class MainActivity extends AppCompatActivity {
     private  int a2;
     private  int apot;
     private String kati;
+    private boolean lastNumeric; // Represent whether the lastly pressed key is numeric or not
+    private boolean lastDot; // If true, do not allow to add another DOT
+    private boolean stateError; // Represent that current state is in error or not
+
+    decimalFormat = new DecimalFormat("#.##########");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -100,124 +106,14 @@ public class MainActivity extends AppCompatActivity {
                 episkopish.setText(episkopish.getText() +"0");
             }
         });
-
-        //κωδικας  μετατροπης του string se int, αναθεση σε μεταβλιτή κα ορισμός αριθμου πραξης
-        sin.setOnClickListener(new View.OnClickListener() {
+        // Decimal point
+        findViewById(R.id.btncomma).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                episkopish2.setText(episkopish.getText() + "+");
-                a1 = Integer.parseInt(episkopish.getText().toString());
-               // apotelesma.setText(episkopish.getText() +"+");
-                episkopish.setText(null);
-                k=1;
-
-
-            }
-        });
-        mion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                episkopish2.setText(episkopish.getText() + "-");
-                a1 = Integer.parseInt(episkopish.getText().toString());
-                // apotelesma.setText(episkopish.getText() +"+");
-                episkopish.setText(null);
-                k=2;
-
-
-            }
-        });
-        epi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                episkopish2.setText(episkopish.getText() + "X");
-                a1 = Integer.parseInt(episkopish.getText().toString());
-                // apotelesma.setText(episkopish.getText() +"+");
-                episkopish.setText(null);
-                k=3;
-
-
-            }
-        });
-        dia.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                episkopish2.setText(episkopish.getText() + "/");
-                a1 = Integer.parseInt(episkopish.getText().toString());
-                // apotelesma.setText(episkopish.getText() +"+");
-                episkopish.setText(null);
-                k=4;
-
-
-            }
-        });
-        //καθαρισμος των textview kai των μεταβλητών
-        clear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                apotelesma.setText(null);
-                episkopish.setText(null);
-                episkopish2.setText(null);
-                a1= 0;
-                a2= 0;
-                k=0;
-
-            }
-        });
-        // ελεγχος πράξης  υπολογισμός πράξης, μετατροπη απο int se string και εισαγωγη του αποτελεσματος
-        // στα textview
-        ison.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                kati = episkopish2.getText().toString();
-                episkopish2.setText( kati + episkopish.getText());
-                a2 = Integer.parseInt(episkopish.getText().toString());
-                episkopish.setText(null);
-
-                if(k==1){
-                    apot= a1 + a2;
-                    apotelesma.setText(String.valueOf(apot));
-                }
-                else if (k == 2){
-                    apot = a1 - a2;
-                    apotelesma.setText(String.valueOf(apot));
-                }
-                else if (k == 3){
-                    apot = a1 * a2;
-                    apotelesma.setText(String.valueOf(apot));
-                }
-                else if (k == 4){
-                    apot = a1 / a2;
-                    apotelesma.setText(String.valueOf(apot));
+                if (lastNumeric && !stateError && !lastDot) {
+                    episkopish.append(".");
+                    lastNumeric = false;
+                    lastDot = true;
                 }
             }
-        });
 
-
-    }
-
-    // συνάρτηση στην οποία γινετε ανάθεση των κουμπιών που δηλώσαμε με τα κουμπιά του layout μεσω των id
-    private void buttons(){
-        one = (Button)findViewById(R.id.btn1);
-        two = (Button)findViewById(R.id.btn2);
-        thre = (Button)findViewById(R.id.btn3);
-        four = (Button)findViewById(R.id.btn4);
-        five = (Button)findViewById(R.id.btn5);
-        six = (Button)findViewById(R.id.btn6);
-        seven = (Button)findViewById(R.id.btn7);
-        eight= (Button)findViewById(R.id.btn8);
-        nine = (Button)findViewById(R.id.btn9);
-        zero = (Button)findViewById(R.id.btnz);
-        epi = (Button)findViewById(R.id.btnx);
-        mion = (Button)findViewById(R.id.btnplin);
-        sin = (Button)findViewById(R.id.btnsin);
-        ison = (Button)findViewById(R.id.btne);
-        clear = (Button)findViewById(R.id.btnc);
-        dia = (Button)findViewById(R.id.btnd);
-        episkopish = (TextView)findViewById(R.id.textView);
-        apotelesma = (TextView)findViewById(R.id.textView2);
-        episkopish2 = (TextView)findViewById(R.id.textView3);
-
-
-
-    }
-}
